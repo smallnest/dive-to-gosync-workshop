47bc7b96b6522acfb5311a55d5f2ee1b2fa4b48f
smallnest <smallnest@gmail.com>
2019-05-07 19:44:41 +0800

    add double-checking race issue

---
+++ 6.happenbefore/double-checking/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var a uint64
+var mu sync.Mutex
+
+func main() {
+	// go foo()
+	// go foo()
+
+	// go fooByAtomic()
+	// go fooByAtomic()
+
+	go fooByMoreScope()
+	go fooByMoreScope()
+
+	time.Sleep(time.Second)
+}
+
+func foo() {
+	if a == 1 {
+		return
+	}
+
+	mu.Lock()
+	if a == 0 {
+		a = 1
+	}
+	mu.Unlock()
+}
+
+func fooByAtomic() {
+	if atomic.LoadUint64(&a) == 1 {
+		return
+	}
+
+	mu.Lock()
+	if a == 0 {
+		atomic.StoreUint64(&a, 1)
+	}
+	mu.Unlock()
+}
+
+func fooByMoreScope() {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if a == 1 {
+		return
+	}
+	a = 1
+}
