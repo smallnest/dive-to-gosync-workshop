254200517878a072a4c5c891e5a1a5a050fb1ad1
chaoyuepan <chaoyuepan@baidu.com>
2022-02-27 17:16:58 +0800

    add barbershop problem

---
+++ 11.classical_problems/barbershop_problem0/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var (
+	seatsLock sync.Mutex
+	seats     int
+
+	cond = sync.NewCond(&seatsLock)
+)
+
+func main() {
+	go barber()
+	go customers()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+}
+
+func randomPause(max int) {
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(max)))
+}
+
+// 理发师
+func barber() {
+	for {
+		// 等待一个用户
+		log.Println("Tony老师尝试请求一个顾客")
+		seatsLock.Lock()
+		for seats == 0 {
+			cond.Wait()
+		}
+		seats--
+		seatsLock.Unlock()
+		log.Println("Tony老师找到一位顾客，开始理发")
+
+		randomPause(2000)
+	}
+}
+
+// 模拟顾客陆陆续续的过来
+func customers() {
+	for {
+		randomPause(1000)
+		customer()
+	}
+}
+
+// 顾客
+func customer() {
+	seatsLock.Lock()
+	defer seatsLock.Unlock()
+
+	if seats == 3 {
+		log.Println("没有空闲座位了，一位顾客离开了")
+		return
+	}
+	seats++
+	cond.Broadcast()
+
+	log.Println("一位顾客开始坐下排队理发")
+}
---
+++ 11.classical_problems/barbershop_problem2/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type Semaphore chan struct{}
+
+func (s Semaphore) Acquire() {
+	s <- struct{}{}
+}
+
+func (s Semaphore) TryAcquire() bool {
+	select {
+	case s <- struct{}{}: // 还有空位子
+		return true
+	default: // 没有空位子了,离开
+		return false
+	}
+}
+
+func (s Semaphore) Release() {
+	<-s
+}
+
+var seats = make(Semaphore, 10)
+
+func main() {
+	// 托尼、凯文、艾伦理发师三巨头
+	go barber("Tony")
+	go barber("Kevin")
+	go barber("Allen")
+
+	go customers()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+}
+
+func randomPause(max int) {
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(max)))
+}
+
+// 理发师
+func barber(name string) {
+	for {
+		// 等待一个用户
+		log.Println(name + "老师尝试请求一个顾客")
+		seats.Release()
+		log.Println(name + "老师找到一位顾客，开始理发")
+
+		randomPause(2000)
+	}
+}
+
+// 模拟顾客陆陆续续的过来
+func customers() {
+	for {
+		randomPause(1000)
+
+		customer()
+	}
+}
+
+// 顾客
+func customer() {
+	if ok := seats.TryAcquire(); ok {
+		log.Println("一位顾客开始坐下排队理发")
+	} else {
+		log.Println("没有空闲座位了，一位顾客离开了")
+	}
+}
