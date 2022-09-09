74a587e82eaac78ac9a3ee6bbbfb44e4a2756848
chaoyuepan <chaoyuepan@baidu.com>
2022-09-09 15:56:14 +0800

    fix the race issue

--- 7.orchestration/fizzbuzz3/fizzbuzz.go
+++ 7.orchestration/fizzbuzz3/fizzbuzz.go
@@ -4,35 +4,15 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"sync/atomic"
 
 	"github.com/marusama/cyclicbarrier"
 )
 
-// 编写一个可以从 1 到 n 输出代表这个数字的字符串的程序，要求：
-
-// 	如果这个数字可以被 3 整除，输出 "fizz"。
-// 	如果这个数字可以被 5 整除，输出 "buzz"。
-// 	如果这个数字可以同时被 3 和 5 整除，输出 "fizzbuzz"。
-// 	例如，当 n = 15，输出： 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz。
-
-// 	假设有这么一个结构体：
-// 	type FizzBuzz struct {}
-// 	func (fb *FizzBuzz) fizz() {}
-// 	func (fb *FizzBuzz) buzz() {}
-// 	func (fb *FizzBuzz) fizzbuzz() {}
-// 	func (fb *FizzBuzz) number() {}
-
-// 请你实现一个有四个线程的多协程版 FizzBuzz，同一个 FizzBuzz 对象会被如下四个协程使用：
-
-// 协程A将调用 fizz() 来判断是否能被 3 整除，如果可以，则输出 fizz。
-// 协程B将调用 buzz() 来判断是否能被 5 整除，如果可以，则输出 buzz。
-// 协程C将调用 fizzbuzz() 来判断是否同时能被 3 和 5 整除，如果可以，则输出 fizzbuzz。
-// 协程D将调用 number() 来实现输出既不能被 3 整除也不能被 5 整除的数字。
-
 type FizzBuzz struct {
 	n int
 
-	v       int
+	v       atomic.Int64
 	barrier cyclicbarrier.CyclicBarrier
 	wg      sync.WaitGroup
 }
@@ -52,7 +32,7 @@ func (fb *FizzBuzz) start() {
 	go fb.fizzbuzz()
 	go fb.number()
 
-	fb.v++
+	fb.v.Add(1)
 
 	fb.wg.Wait()
 }
@@ -64,7 +44,7 @@ func (fb *FizzBuzz) fizz() {
 	for {
 		fb.barrier.Await(ctx)
 
-		v := fb.v
+		v := int(fb.v.Load())
 
 		if v > fb.n {
 			return
@@ -81,7 +61,7 @@ func (fb *FizzBuzz) fizz() {
 				fmt.Print(" fizz,")
 			}
 
-			fb.v++
+			fb.v.Add(1)
 		}
 
 	}
@@ -94,7 +74,7 @@ func (fb *FizzBuzz) buzz() {
 	for {
 		fb.barrier.Await(ctx)
 
-		v := fb.v
+		v := int(fb.v.Load())
 
 		if v > fb.n {
 			return
@@ -111,7 +91,7 @@ func (fb *FizzBuzz) buzz() {
 				fmt.Print(" buzz,")
 			}
 
-			fb.v++
+			fb.v.Add(1)
 		}
 	}
 }
@@ -122,7 +102,7 @@ func (fb *FizzBuzz) fizzbuzz() {
 	for {
 		fb.barrier.Await(ctx)
 
-		v := fb.v
+		v := int(fb.v.Load())
 
 		if v > fb.n {
 			return
@@ -135,7 +115,7 @@ func (fb *FizzBuzz) fizzbuzz() {
 				fmt.Print(" fizzbuzz,")
 			}
 
-			fb.v++
+			fb.v.Add(1)
 			continue
 		}
 	}
@@ -147,7 +127,7 @@ func (fb *FizzBuzz) number() {
 	for {
 		fb.barrier.Await(ctx)
 
-		v := fb.v
+		v := int(fb.v.Load())
 
 		if v > fb.n {
 			return
@@ -160,7 +140,7 @@ func (fb *FizzBuzz) number() {
 				fmt.Printf(" %d,", v)
 			}
 
-			fb.v++
+			fb.v.Add(1)
 			continue
 		}
 
